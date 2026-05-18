1163e2134cc5e6474650ed13777a85b902d56ab8
ofmarin <ofelmaran@gmail.com>
2026-05-18 14:44:43 -0400

    add cart component, and some custom variables to global css

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Cart } from '../cart/cart'
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [Cart],
   templateUrl: './card.html',
   styleUrls: ['./card.css'],
 })
