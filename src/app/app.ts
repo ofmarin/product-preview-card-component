c683fdc864f026c330180b491ee68f7a54893754
ofmarin <ofelmaran@gmail.com>
2026-05-17 21:11:40 -0400

    add card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Card} from './card/card'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Card],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
+  attribution = "https://www.frontendmentor.io/profile/ofmarin"
   protected readonly title = signal('product-preview-card-component');
 }
